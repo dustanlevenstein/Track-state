22cb0ee9605b6b96706d77afdda73568cf59e081
Dustan Levenstein <dustanl@math.ucla.edu>
2019-03-04 10:27:40 -0600

    Updated settings and framework.

--- tracker.py
+++ tracker.py
@@ -1,4 +1,15 @@
-import subprocess
-import time
+import subprocess, time, sys
+from subprocess import PIPE, STDOUT, DEVNULL
 
+settings_f = open("settings.txt", "r")
+settings = settings_f.read()
+exec(settings)
+# At this point, the variables subprocess_args, subprocess_kargs,
+# period, timestamp_output, stdout, stderr, and terminating_condition
+# should be defined.
 
+if terminating_condition is None:
+	terminating_condition = lambda *args, **kargs: None
+
+while True:
+	
